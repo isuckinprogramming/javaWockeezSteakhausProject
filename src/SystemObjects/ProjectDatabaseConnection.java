9f89c058ae5fe9fa682d90cbd7c2d4b4bee61311
Borgs <nisiyawa55@gmail.com>
2023-09-24 09:55:55 -0700

    Added Reservation Obj to represent DB table
    This commit is created inside a branch,I want to test out how to
    work with branches. Trying out fast branching.

---
+++ src/SystemObjects/ProjectDatabaseConnection.java
@@ -0,0 +1,10 @@
+package SystemObjects;
+
+public class ProjectDatabaseConnection {
+
+  
+  
+
+
+
+}
---
+++ src/SystemObjects/Reservation.java
@@ -0,0 +1,39 @@
+package SystemObjects;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import DatabaseObjectTemplates.DBEntity;
+
+public class Reservation implements DBEntity {
+
+  @Override
+  public String getStringSQLQuery() {
+    return "CREATE TABLE Reservations (  \r\n" + //
+        "  ReservationID INT AUTO_INCREMENT Primary Key, \r\n" + //
+        "  ReservationDateTime DATETIME NOT NULL, \r\n" + //
+        "  PartySize INT DEFAULT 1,\r\n" + //
+        "  TableNumber INT NOT NULL, \r\n" + //
+        "  Status ENUM('Confirmed','Pending','Cancelled') DEFAULT 'pending'\r\n" + //
+        ")";
+  }
+
+  @Override
+  public void CreateTable(Connection connection) {
+    
+    try {
+      Statement statement = connection.createStatement();
+      
+      statement.execute( getStringSQLQuery() );
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void truncateTable(  ) {
+  // NO IMPLEMENTATION YET.
+  }
+}
