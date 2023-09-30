d4361ce427c3d67640c8c58c0ead8156af602b43
Borgs <nisiyawa55@gmail.com>
2023-09-29 21:02:02 -0700

    Created Base Class for User & DB Bridge
    Created Class to hold all User Side And Program Runtime Methods.
    
    Renamed the Class for testing Development features
    as ApplicationDevelopmentFeaturesTester.

---
+++ src/SystemObjects/ProgramRuntime.java
@@ -0,0 +1,27 @@
+package SystemObjects;
+
+
+
+/***
+ * Class contains the method to run when program starts and
+ * handles the runtime methods of the Program.
+ * 
+ * */ 
+public class ProgramRuntime {
+  
+
+  public static void main(String[] args) {
+
+    ProgramRuntime programRunning = new ProgramRuntime();
+    
+
+  }
+
+  private void start() {
+  
+  
+  }
+
+
+
+}
---
+++ src/SystemObjects/ProgramUser.java
@@ -0,0 +1,10 @@
+package SystemObjects;
+
+
+/***
+ * Represents the user of the program and receives the data
+ * intended for the user of the program.
+ * */ 
+public class ProgramUser {
+  
+}
